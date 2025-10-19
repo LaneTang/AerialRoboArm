4a7dc52755fc846468d29d971d8ff993f4c691ff
LaneTang <147981118+LaneTang@users.noreply.github.com>
2025-10-20 00:35:21 +0800

    分割本地环境

---
+++ toolchain.template.cmake
@@ -0,0 +1,6 @@
+# 工具链模板：请复制为toolchain.cmake并修改路径
+set(CMAKE_C_COMPILER "/path/to/arm-none-eabi-gcc")
+set(CMAKE_CXX_COMPILER "/path/to/arm-none-eabi-g++")
+set(CMAKE_ASM_COMPILER "/path/to/arm-none-eabi-gcc")
+set(CMAKE_OBJCOPY "/path/to/arm-none-eabi-objcopy")
+# ... 其他工具链配置
